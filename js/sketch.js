5dfd748a4bf220be41e45177e2147aab13487139
Alex Cent <alex_cent@mailbox.org>
2022-08-03 20:32:53 +0300

    Enable drawing on canvas

--- js/sketch.js
+++ js/sketch.js
@@ -8,10 +8,22 @@ function makeArea() {
         squares.push(createSquare());
     }
     area.replaceChildren(...squares);
+    enableDraw();
 }
 
 function createSquare() {
     const square = document.createElement('div');
     square.classList.add('unit');
     return square;
+}
+
+function enableDraw(func = draw) {
+    const squares = document.getElementsByClassName('unit');
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].onmouseenter = (() => func(squares[i]));
+    }
+}
+
+function draw(obj) {
+    obj.style['background-color'] = 'black';
 }
